62d660abb2a2165f71fb70a5f9d846cbfada55be
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-10-13 14:01:09 -0500

    need to fix issues

--- ilstu/Flight.java
+++ ilstu/Flight.java
@@ -15,7 +15,7 @@ package ilstu;
 public class Flight {
     // instance variable
     private String flightNum; // A flight “number” that is two uppercase letters followed by 3 or 4 digits
-    private int aiportCode; // The 3-letter airport code where the flight originates
+    private String airportCode; // The 3-letter airport code where the flight originates
     private String destAirport; // The 3-letter code for the destination airport
     private String aircraft; // An aircraft type that is a string of characters
     private int maxNumSeats; // The maximum number of seats available on this flight
@@ -28,41 +28,94 @@ public class Flight {
     private static final int ac2Capacity = 216;
     private static final int ac3Capacity = 50;
 
-    // The Flight class must have a constructor that receives the flight number,
-    // origin airport, destination airport and aircraft type. It should initialize
-    // those four instance variables and set the number of seats currently reserved
-    // to zero.
-
     /**
      * Constructor Method
      * 
-     * @param
+     * @param flightNum   -- the flight number
+     * @param airportCode -- the origin airport
+     * @param destAirport -- destination airport
+     * @param aircraft    -- aircraft type
+     * 
+     *                    function: constructor that receives the flight number,
+     *                    origin airport, destination airport, and aircraft type. It
+     *                    also sets the number of seats currently reserved to zero.
      */
-    public Flight(String flightNum, int airportCode, String destAirport, String aircraft) {
-
+    public Flight(String flightNum, String airportCode, String destAirport, String aircraft) {
+        this.flightNum = flightNum;
+        this.airportCode = airportCode;
+        this.destAirport = destAirport;
+        this.aircraft = aircraft;
+        numSeatsReserved = 0;
+        calcMaximumSeats(aircraft);
     }
 
-    // calcMaximumSeats method. The calcMaximumSeats method will initialize the
-    // maximum number of seats available based on the aircraft type.
+    /**
+     * Overridden toString method
+     * 
+     * @return a string containing the flight number, originating airport code,
+     *         destination airport code, and the number of seats avialable
+     */
+    @Override
+    public String toString() {
+        return flightNum + "\t" + airportCode + "\t" + destAirport + "\t" + getSeatsAvailable() + " seats available";
+    }
 
-    // The Flight class must have the following public methods.
-    // A toString method that returns a String with the flight number, origin
-    // airport, destination airport, and number of seats available (format shown in
-    // the sample output below).
-    // A getSeatsAvailable method that returns the difference between the maximum
-    // number of seats and the number of seats currently reserved.
+    /**
+     * Getter for the number of seats available
+     * 
+     * @return the difference between the maximum number of seats and the number of
+     *         seats currently reserved
+     */
+    public int getSeatsAvailable() {
+        return maxNumSeats - numSeatsReserved;
+    }
 
-    // A reserve method that receives an int parameter representing the number of
-    // seats to reserve. If the number of seats to reserve is less than or equal to
-    // the number of seats available, the method should increase the number of seats
-    // currently reserved by the appropriate number and return true. If not, the
-    // method should return false and leave the number of seats unchanged.
+    /**
+     * Getter for the flight number
+     *
+     * @return flight number
+     */
+    public String getFlightNum() {
+        return flightNum;
+    }
 
-    // The Flight class must have the following private method.
-    // A calcMaximumSeats method that sets the flight’s maximum number of seats
-    // instance variable based on the aircraft type. The maximum number of seats for
-    // different aircraft types are shown in the following table. If the String
-    // parameter does not match a valid aircraft type, the maximum number of seats
-    // should be set to zero.
+    // /**
+    // * Reserve seats method
+    // *
+    // * @param reserveSeats -- the number of seats needed to be reserved
+    // *
+    // * @return true if the number of seats are available and reserved or false if
+    // * the number of seats are unavailable
+    // */
+    // public boolean reserveSeats(int reserveSeats) {
+    // if (getSeatsAvailable() < reserveSeats) {
+    // numSeatsReserved = reserveSeats;
+    // return true;
+    // }
+    // return false;
+    // }
+    public int reserveSeats(int reserveSeats) {
+        if (getSeatsAvailable() >= reserveSeats) {
+            numSeatsReserved = reserveSeats;
+        }
+        return maxNumSeats - numSeatsReserved;
+    }
 
+    /**
+     * Calculates the number of seats available based on the aircraft
+     * 
+     * @param aircraft
+     */
+    private void calcMaximumSeats(String aircraft) {
+        if (aircraft.equals(ac1)) {
+            maxNumSeats = ac1Capacity;
+        } else if (aircraft.equals(ac2)) {
+            maxNumSeats = ac2Capacity;
+        } else if (aircraft.equals(ac3)) {
+            maxNumSeats = ac3Capacity;
+        } else {
+            maxNumSeats = 0;
+        }
+
+    }
 }
