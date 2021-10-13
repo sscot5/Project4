eac7e0b64a8b7c2e2527b51f03f4f24195b022d1
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-10-13 09:19:42 -0500

    update

---
+++ Airport.java
@@ -0,0 +1,11 @@
+/*
+ * Filename: Airport.java Last modified on October 12, 2021
+ * 
+ * Course: IT 168 Class Section: 1
+ */
+
+/**
+ * This is a simple airport class
+ * 
+ * @author Stephanie Scott
+ */
